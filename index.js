b3ecf2bfb35574940d5236fbdc29030291a3cdb6
TristanVarewijck <tristanconnect4@outlook.com>
2021-07-08 11:18:52 +0200

    detial page in progress

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const colors = require('colors'); 
 const express = require('express'); 
 const app = express(); 
-const port = 3000; 
+const port = 5000; 
 let methodOverride = require('method-override')
 const Book = require('./models/Book')
 // const File = require("./models/upload");
